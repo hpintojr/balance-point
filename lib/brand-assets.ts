bf33c9368abd71933615bff1e1475b9b0510a7f6
hpintojr <hpintojr@gmail.com>
2026-09-07 19:40:46 -0700

    Restore booking design and update approved hero

---
+++ lib/brand-assets.ts
@@ -0,0 +1,9 @@
+import { heroRef1 } from "./hero-ref-1";
+import { heroRef2 } from "./hero-ref-2";
+import { heroRef3 } from "./hero-ref-3";
+import { realLogo1 } from "./real-logo-1";
+import { realLogo2 } from "./real-logo-2";
+import { realLogo3 } from "./real-logo-3";
+
+export const heroReferenceDataUri = `data:image/webp;base64,${heroRef1}${heroRef2}${heroRef3}`;
+export const realLogoDataUri = `data:image/jpeg;base64,${realLogo1}${realLogo2}${realLogo3}`;
